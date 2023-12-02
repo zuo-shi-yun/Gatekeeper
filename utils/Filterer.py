4f1ffe7255cbbb5e0404d8eea3c60bcc4d0dd295
zuo-shi-yun <2818252801@qq.com>
2023-12-02 16:11:23 +0800

    feat-插件初始化、数据库、配置文件

--- utils/Filterer.py
+++ utils/Filterer.py
@@ -1,15 +1,19 @@
 """实现黑白名单、临时用户逻辑"""
 
+from utils.database import MyDict
+
 
 class HandleRequest:
     """处理请求"""
 
-    def __init__(self, **kwargs):
+    def __init__(self, cfg: MyDict, **kwargs):
         self.qq = kwargs.get('launcher_id')
         self.sender_id = kwargs.get('sender_id')
         self.qq_type = 0 if kwargs.get('launcher_type') == 'person' else 1  # 0 个人 1 群
 
         self.e = None  # 异常
+
+        self.cfg = cfg
 
         self.status = self.handle()  # True放行False阻止
 
--- utils/cmd.py
+++ utils/cmd.py
@@ -1,19 +1,24 @@
 """处理用户指令"""
+from pkg.plugin.host import PluginHost
+
+from utils.database import MyDict
 
 
 class HandleCmd:
     """处理用户指令"""
 
-    def __init__(self, cmd: str, param: list, **kwargs):
+    def __init__(self, cfg: MyDict, cmd: str, param: list, **kwargs):
         self.qq = kwargs.get('launcher_id')  # 发起者id,用户qq号或群qq号
         self.sender_id = kwargs.get('sender_id')  # 发送者id
         self.launcher_type = 0 if kwargs.get('launcher_type') == 'person' else 1  # 消息类型:0个人1群
-
-        self.ret_msg = ''  # 回复信息
-        self.e = None  # 异常
+        self.host = PluginHost()
 
         self.cmd = cmd  # 用户命令
         self.param = param  # 命令参数
+        self.cfg = cfg  # 配置
+
+        self.ret_msg = ''  # 回复信息
+        self.e = None  # 异常
 
         self.had_handle_cmd = self.handle()  # 处理流程
 
