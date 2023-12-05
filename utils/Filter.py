9fa24e93ee40895d3b05ee467a1a30caa203e133
zuo-shi-yun <2818252801@qq.com>
2023-12-05 12:58:03 +0800

    chore-更新系统流程

--- utils/Filter.py
+++ utils/Filter.py
@@ -55,8 +55,8 @@ class HandleRequest:
         svc = DatabaseManager()
         qq_list = svc.query(['qq'])  # 临时用户名单
 
-        if self.qq in qq_list:  # 在临时用户数据库中
-            use_time = svc.query(['time'], {'qq': self.qq})[0]  # 上次使用时间
+        if self.sender_id in qq_list:  # 在临时用户数据库中
+            use_time = svc.query(['time'], {'qq': self.sender_id})[0]  # 上次使用时间
             if self.is_last_use_time_out_refresh_time(self.cfg['tourist_refresh_days'], use_time):  # 若上次使用已过刷新时间范围
                 current_time = datetime.datetime.now()
                 formatted_time = current_time.strftime("%m-%d")
@@ -66,17 +66,17 @@ class HandleRequest:
                 else:
                     max_usage = self.cfg['tourist_max_usage']
                 # 更新数据库
-                svc.update({'time': formatted_time, 'usage': 1, 'max_usage': max_usage}, {'qq': self.qq})
+                svc.update({'time': formatted_time, 'usage': 1, 'max_usage': max_usage}, {'qq': self.sender_id})
                 return True
             else:  # 上次使用未过刷新时间范围
-                use_time = svc.query(['usage'], {'qq': self.qq})[0]
+                use_time = svc.query(['usage'], {'qq': self.sender_id})[0]
                 max_use_time = self.cfg['tourist_max_usage']
 
                 if use_time >= max_use_time:  # 已经超出则禁止使用
                     self.ret_msg = self.cfg['tourist_over_usage_msg'].format(self.cfg['tourist_refresh_days'])
                     return False
                 else:  # 没有超出则允许使用并更新使用次数
-                    svc.update({'usage': use_time + 1}, {'qq': self.qq})
+                    svc.update({'usage': use_time + 1}, {'qq': self.sender_id})
                     return True
         else:  # 没有在则插入并设次数为1
             if self.cfg['tourist_random_usage']:  # 开启了随机配额:
@@ -85,7 +85,7 @@ class HandleRequest:
                 max_usage = self.cfg['tourist_max_usage']
             # 插入数据库
             svc.insert(
-                {'qq': self.qq, 'time': datetime.datetime.now().strftime("%m-%d"), 'usage': 1,
+                {'qq': self.sender_id, 'time': datetime.datetime.now().strftime("%m-%d"), 'usage': 1,
                  'max_usage': max_usage})
             return True
 
